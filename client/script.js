a2df56c52eb53fa3c46b3cca7f4c78634dfd089a
ASHRIBAD PANDA <133385084+ashribad15@users.noreply.github.com>
2024-07-13 23:17:41 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5173/', { // change this to your server URL
+    const response = await fetch('https://codex-hb6u.onrender.com', { // changed this to your server URL
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
